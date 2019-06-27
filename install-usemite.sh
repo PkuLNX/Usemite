182653a0c780d1008e90487e5132df2c67e57873
LNX#.CREW <50340591+PkuLNX@users.noreply.github.com>
2019-06-28 04:02:15 +0700

    Update install-usemite.sh

--- install-usemite.sh
+++ install-usemite.sh
@@ -3,11 +3,11 @@ reset
 echo ""
 echo "> Please wait to Installing Usemite .."
 sleep 2
-sudo mv /settings/usemite /usr/bin/
-sudo chmod +x /usr/bin/usemite
+sudo cd settings
+chmod +x usemite
 sudo apt-get update
 clear
 echo ""
 echo "> Installing was Finish !!"
 sleep 3
-sudo /usr/bin/usemite
+./usemite
